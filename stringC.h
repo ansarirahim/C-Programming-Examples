8bba0366458d2269553d54dc01449d788814cf50
AbdulRaheem <ansarirahim1@gmail.com>
2020-06-03 23:41:09 +0530

    Create stringC.h
    Header file

---
+++ stringC.h
@@ -0,0 +1,6 @@
+void trimLeading(char *s);
+int checkAnyNonNumericCharInArray(char *s);
+void checkPrimeNumberInArray(char *numberString);
+void ClearScreen(int os);
+#define __WINDOWS__ 0
+#define __LINUX__ 1
